29f563c4a7e36dee8861733881f9a1cffb4bf03d
AndreNunes1812 <andre.nunes020770@gmail.com>
2020-11-30 21:20:57 +0100

    replace fileName arquivos

---
+++ concatenarArquivos.js
@@ -0,0 +1,159 @@
+import parquet from 'parquetjs';
+import { dirname, join } from 'path' 
+import { promisify } from 'util'
+import { promises, createReadStream, createWriteStream } from 'fs'
+import { pipeline, Transform } from 'stream'
+const pipelineAsync = promisify(pipeline)
+
+import csvtojson from 'csvtojson'
+import jsontocsv from 'json-to-csv-stream'
+
+import StreamConcat from 'stream-concat'
+
+const { readdir } = promises
+import debug from 'debug'
+import { STATUS_CODES } from 'http'
+const log = debug('app:concat')
+
+
+const { pathname: currentFile } = new URL(import.meta.url)
+const cwd = dirname(currentFile)
+const filesDir = `${cwd}/files`
+const output = `${cwd}/output/arquivos.txt`
+
+
+export async function  concatenarArquivos() {
+
+  const files = (await readdir(filesDir))
+
+  const streams = files.map(
+      item => createReadStream(join(filesDir, item))
+  )
+
+  const combinedStreams = new StreamConcat(streams)
+
+  const finalStream = createWriteStream(output)
+
+  const  handleStream = new  Transform({
+      transform: async  (chunk, encoding, cb) => {
+          const data = (chunk)
+
+        /* csvtojson({
+            noheader: true,
+          //  output: 'json'
+          }).fromString(`${(chunk)}`)
+            .then( async row => {
+              const ww3 = row.map(r => {
+                const tt = r.field1.split(';')
+                
+                gravarParquet(
+                  tt[0],
+                  tt[1],
+                  tt[2],
+                  tt[3],
+                  tt[4],
+                  tt[5],
+                  tt[6],
+                  tt[7],
+                  tt[8],
+                  tt[9],
+                  tt[10],
+                  tt[11],
+                  tt[12],
+                  tt[13],
+                  tt[14],
+                  tt[15],
+                  tt[16],
+                  tt[17],
+                  tt[18],
+                )
+                            
+              })
+            })
+        */
+          return cb(null, JSON.stringify(`${chunk}`))
+    }
+})
+
+await pipelineAsync(
+    combinedStreams,
+    handleStream,
+    finalStream
+)
+
+log(`${files.length} files merged! on ${output}`)
+
+  return true
+}
+
+async function gravarParquet(nameRow,calling_numberRow,called_numberRow,serial_numberRow,
+  call_typeRow,
+  call_durationRow,
+  start_timeRow,
+  imsiRow,
+  switchRow,
+  cell_inRow,
+  cell_outRow,
+  tecnologiaRow,
+  file_nameRow,
+  first_lacRow,
+  last_lacRow,
+  ggsn_addressRow,
+  dateRow,
+  in_stockRow
+  
+  ) {
+
+  var schema = new parquet.ParquetSchema({
+    name: { type: 'UTF8' },
+    exchange_id: { type: 'UINT_64' },
+    calling_number: { type: 'UINT_64' },
+    called_number:  {  type: 'UINT_64' },
+    serial_number:  {  type: 'UINT_64' },
+    call_type: { type:'UINT_64'},
+    call_duration: { type:'UINT_64'},
+    start_time: { type: 'TIMESTAMP_MILLIS'},
+    imsi: { type: 'UTF8'},
+    switch: {type: 'UINT_64'},
+    cell_in: { type: 'UTF8'},
+    cell_out: {type: 'UTF8'},
+    tecnologia: { type: 'UTF8'},
+    file_name: { type: 'UTF8'},
+    first_lac: {type: 'UTF8'},
+    last_lac: { type: 'UTF8'},
+    ggsn_address: { type: 'UTF8'},
+    date: { type: 'TIMESTAMP_MILLIS' },
+    in_stock: { type: 'BOOLEAN' }
+    
+  });
+  
+  const writeRow = await parquet.ParquetWriter.openFile(schema, './output/filesystem.parquet');
+
+  await writeRow.appendRow( {
+    name: nameRow,
+    exchange_id: nameRow ,
+    calling_number: calling_numberRow,
+    called_number: called_numberRow,
+    serial_number:  serial_numberRow,
+    call_type: call_typeRow,
+    call_duration: call_durationRow,
+    start_time: start_timeRow ,
+    imsi: imsiRow,
+    switch: switchRow  ,
+    cell_in: cell_inRow ,
+    cell_out: cell_outRow ,
+    tecnologia: tecnologiaRow ,
+    file_name: file_nameRow,
+    first_lac: first_lacRow,
+    last_lac: last_lacRow ,
+    ggsn_address: ggsn_addressRow ,
+    date: dateRow ,
+    in_stock: in_stockRow
+
+  } )
+
+  await writeRow.close();
+
+  return true 
+}
+
--- server.js
+++ server.js
@@ -1,35 +1,9 @@
 import express  from 'express';
 import parquet from 'parquetjs';
-import { readwrite } from './readwrite.js';
+import { concatenarArquivos } from './concatenarArquivos.js';
 
 const PORT = 8080;
 const HOST = '0.0.0.0';
-
-
-
-readwrite()
-
-
-/*EXCHANGE_ID    = 8138;
-CALLING_NUMBER = 5521987366501;
-CALLED_NUMBER  = 5521908100740;
-SERIAL_NUMBER  = 5604248321560188;
-CALL_TYPE      = 1;
-CALL_DURATION  = 98;
-START_TIME     = 2019-10-05T05:56:17;
-IMSI           = 8136378640573727;
-SWITCH         = 39;
-CELL_IN        = 65234;
-CELL_OUT       = 00056;
-TECNOLOGIA     = A;
-FILE_NAME      = calls.txt;
-FIRST_LAC      = f86a89;
-LAST_LAC       = bf1043;
-GGSN_ADDRESS   = endpoint.ggsn.tatic.com
-*/
-
-// declare a schema for the `CDR` table
-
 
 
 var schema = new parquet.ParquetSchema({
@@ -90,141 +64,12 @@ try {
   }
 )
 
-
-app.get('/gerar_parquet', async(req, res) => {
-
-  let valor = []
-  let ValorJson= []
-  let arquivosTodos = []
-
-  const arquivos =   await fs.promises.readdir('./files',function(error,files){});
-
- // console.log('Arquivos:',arquivos)
-  
- // let readFiles = await fs.promises.readFile('./files/' + arquivos[0],'utf8');
- // arquivosTodos.push(readFiles);
-
- // readFiles = await fs.promises.readFile('./files/' + arquivos[1],'utf8');
- // arquivosTodos.push(readFiles);
-
- // console.log('Todos:',arquivosTodos)
-
-
-  // const writeFiles = await fs.promises.writeFile("./output/filesystem.parquet","console.log('Andre Nunes')")
-
-  var writeRow = await parquet.ParquetWriter.openFile(schema, './output/filesystem.parquet');
-  
-  const file = await fs.readFile('./files/' + arquivos[0], 'utf8', async function (err, data) {
-
-    try {
-      
-      let zero=0;
-      var linhas = data.split(/\r?\n/);
-      linhas.forEach(async function (linha) {
-
-        //  console.log('Estou aqui')        
-        if (linhas.length >= zero) {
-        
-           console.log('N:', zero)
-
-           const linhaSplit = linha.split(';')
-
-           valor.push( linhaSplit );
+app.get('/concatenar', async(req, res) => {
  
-           let nameRow= valor[zero][0] 
-           let exchange_idRow= valor[zero][0]
-           let calling_numberRow = valor[zero][1]
-           let called_numberRow = valor[zero][2]
-
-         
-           ValorJson.push({
-              name: nameRow,
-              exchange_id: exchange_idRow ,
-              calling_number: calling_numberRow,
-              called_number: called_numberRow
-           })
-
-           zero++
-        
-            Promise.all( await writeRow.appendRow( {
-              name: nameRow,
-              exchange_id: exchange_idRow ,
-              calling_number: calling_numberRow,
-              called_number: called_numberRow
-            } )
-            
-           );
-           await writeRow.close(); 
-
-         
-
-           //console.log('*******************************')
-           //console.log(`N:${zero}`, valor[zero][0])
-           //console.log(`N:${zero}`, valor[zero][1])
-           //console.log(`N:${zero}`, valor[zero][2])
-           //console.log('*******************************')
-      
-       
-           
-          
-//          await writeRow.appendRow({
-//            name: nameRow,
-//            exchange_id: exchange_idRow ,
- //           calling_number: calling_numberRow,
- //           called_number: called_numberRow
-  //        });
-
-    //      await writeRow.close();
-          
-        }
-      });
-
-
-    } catch (error) {
-      console.log('Error:', error);
-
-    } finally {
-     
-//  console.log('Sai:', JSON.stringify( ValorJson).replace(']','').replace('[',''));
-    // await writeRow.appendRow( JSON.stringify( ValorJson).replace(']','').replace('[','') );
-     
-    }
-
-    console.log('Saiiiiiiiiiiiiiiii');
-
-    // console.log('Valoroooo:', valor[0][0],valor[0][1])
-    // console.log('Data:', data.split(';'))
-    //   arquivos = data.split(';')
-    /*  fs.promises.writeFile('./output/filesystem.txt', data, function(err, result) {
-         if(err) console.log('error', err);
-       });
-       */
-  });
-
-
- // console.log('File:', JSON.stringify(file))
-
-
-//AVRO
-  
-  
- /*const write = await fs.promises.writeFile('./output/filesystem.txt', XPTO, function(err, result) {
-    if(err) console.log('error', err);
-  });
-*/
-
-  // console.log('Write:', write)
-  
-/*  try {
-    await fs.writeFile("/output/filesystem.txt", "console.log('Hello world with Node.js v13 fs.promises!'");
-    console.info("File created successfully with Node.js v13 fs.promises!");
-  } catch (error){  
-    console.error(error);
-  }
-*/
+  concatenarArquivos()
 
   res.contentType('application/json');
-  return res.send(JSON.stringify(arquivos));
+  return res.send({message: 'arquivo sendo concatenado....'});
 });
 
 app.listen(PORT, HOST);
